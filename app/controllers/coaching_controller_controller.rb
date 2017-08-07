2a322add62a8473c2dd911c7d795db01ad675e39
David Michel <davidmichel77@gmail.com>
2017-08-07 14:15:15 +0200

    completed exercise

---
+++ app/controllers/coaching_controller_controller.rb
@@ -0,0 +1,41 @@
+class CoachingControllerController < ApplicationController
+
+  def answer
+    @question = params[:query]
+    @answer = coach_answer(@question)
+  end
+
+  def ask
+
+  end
+
+  def coach_answer(your_message)
+    # TODO: return coach answer to your_message
+    if your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    elsif your_message != "I am going to work right now!"
+      return "I don't care, get dressed and go to work!"
+    elsif your_message == "I am going to work right now!"
+      return ""
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    # TODO: return coach answer to your_message, with additional custom rules of yours!
+    while your_message == your_message.upcase
+      if your_message.include?("?")
+        return "I can feel your motivation!" + " " + "Silly question, get dressed and go to work!"
+      elsif your_message != "I AM GOING TO WORK RIGHT NOW!"
+        return "I can feel your motivation!" + " " + "I don't care, get dressed and go to work!"
+      elsif your_message == "I AM GOING TO WORK RIGHT NOW!"
+        return ""
+      end
+    end
+    coach_answer(your_message)
+  end
+
+end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'coaching_controller/answer'
+
+  get 'coaching_controller/ask'
+
+  get 'answer', to: 'coaching_controller#answer'
+  get 'ask', to: 'coaching_controller#ask'
+
+  root to: 'coaching_controller#ask'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
